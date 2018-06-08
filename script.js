d1cb8a8dcf0f734d1152d2bb74fc324a0b07782c
Neacsu Nicusor <37114509+nn007@users.noreply.github.com>
2018-06-08 16:05:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 $(document).ready(function() {
 
 	$('.FB_reactions').facebookReactions({});
+	$('body, html').css({
+		'opacity': 1,
+		'display': 'block',
+		'transition': 'all 1s ease'
+	});
+	//use mouse or enter 
+	$('#button').keypress(function (e) {
+		e.preventDefault();
+	  if (e.which == 13) {
+	    $(this).click();
+	    return false;    
+	  }
+	});
+	$('#add').keypress(function (x) {
+		x.preventDefault();
+	  if (x.which == 13) {
+	    $(this).click();
+	    return false;    
+	  }
+	});
 });
 
 //load image from the user PC
@@ -19,33 +39,54 @@ function showImage() {
 	}
 }
 
-//add content
+//take screenshot
+function screenshot(box){
+	//copy the webpage
+	var restorpage  = document.body.innerHTML;
+	//get the specific content that you want to print
+	var content = document.getElementById('box').innerHTML;
+	//body gets only the specific content
+	document.body.innerHTML = content;
+	//run the window print
+	window.print();
+	//restore the body webpage after the print
+	document.body.innerHTML = restorpage;
+}
+
+//add content and validation
 $('#button').on('click',function(){
 
 	//set up all the varibles
-	var date = new Date();
 	var name = $('#name').val();
 	var color = $("#color").val();
 	var message = $('#message').val();
+	var file = $('#file').val();
+	var date = new Date();
 
-	//add the variables to the html content
-	$("#Name").text(name);
-	$("#my-message").text(message).css({"color": color});
-	if(date.getMinutes()<10){
-		$("#minutes").text(': 0'+date.getMinutes());
-	}else{
-		$("#minutes").text(': '+date.getMinutes());
+	if(name && color && message && file){
+		
+		//add the variables to the html content
+		$("#Name").text(name);
+		$("#my-message").text(message).css({"color": color});
+		if(date.getMinutes()<10){
+			$("#minutes").text(': 0'+date.getMinutes());
+		}else{
+			$("#minutes").text(': '+date.getMinutes());
+		}
+		
+		$("#hours").text(date.getHours());
+
+		//show the last container when the button is clicked
+		$(".container:last").removeClass("hidden");
 	}
-	
-	$("#hours").text(date.getHours());
-
-	//show the last container when the button is clicked
-	$(".container:last").removeClass("hidden");
+	else{
+		alert("Toate campurile sunt necesare :)");
+	}
 
 });
 
 //show input comment when the 'comment' is clicked
-$('#comment').on('click',(a)=>{
+$('#comment').on('click',function(a){
 	a.preventDefault();
 	$('.comment-section').toggleClass('hidden');	
 });
@@ -63,18 +104,32 @@ var arr = [
 //add comment
 $('#add').on('click',(e)=>{
 	e.preventDefault();
-	var text = $('#text').val();
-	var target = $('#content');
-	var li = $('<li></li>');
-	li.text(text);
-	target.append(li);
-	document.getElementById("text").value='';
-	$('.comment-section').addClass('hidden');	
 
 	//take a random image from array
 	var x = Math.floor(Math.random()*arr.length);
+
+	//take all the values and create all the elemnts
+	var myText = $('#text').val();
+	var target = $('#content');
+	var li = $('<li></li>');
+	var image = $('<img/>');
+
 	//wait 2s before image reaction is added
 	setTimeout(function(){
-		var y = $('#commentIcon').attr('src',arr[x]);
+		//set the source to image
+		image.attr('src',arr[x]);
+		image.attr('alt',"");
+		
 	},2000);
+
+	//append content to the list item
+	li.text(myText).append(image);
+
+	//append everything to the specific target
+	target.append(li);
+	
+	document.getElementById("text").value='';
+	$('.comment-section').addClass('hidden');
 });
+
+
